2ec30ab3843c3e97a8ca54bce339b2629f71bad6
kunalpan <panchalkunal9498@gmail.com>
2019-03-24 17:59:24 +0530

    files added

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import '/style/index.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const wow = new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    });
+    wow.init();
+});
