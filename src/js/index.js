13cc7d27dd91bbfe72e5ff467c36ac01b2537cb2
Kunal Panchal <31957053+kunalpan23@users.noreply.github.com>
2019-03-31 03:45:54 +0530

    Builds (#5)
    * build changes done
    
    * pushed to commit
    
    * changes done
    
    * build changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import '/style/index.scss';
+// import AppendImages from './AppendGallery';
 
 const trackerElem = $('[a-tracker]'); // Navigation bar Elems
 const headerElem = $('[a-header]'); //Header Elem Selector
+const hamburger = document.querySelector('[a-hamburger]');
+const listElem = document.querySelector('[a-nav]');
 
 $(window).on('load', e => {
+    // AppendImages(); //This Function is to append the images
+
     const wow = new WOW({
         boxClass: 'wow', // default
         animateClass: 'animated', // default
@@ -13,11 +18,22 @@ $(window).on('load', e => {
     });
     wow.init();
 
+    // $('.pics').glisse({
+    //     changeSpeed: 550,
+    //     speed: 500,
+    //     effect: 'bounce',
+    //     fullscreen: false
+    // });
+
     trackerElem.bind('click', function(e) {
         e.preventDefault(); // prevent hard jump, the default behavior
         const THIS = $(this);
         // const target = $(THIS.attr('a-tracker'));
         const target = THIS.attr('a-tracker');
+        if (window.innerWidth < 570) {
+            listElem.classList.toggle('active');
+            document.querySelector('.icon-one').classList.toggle('active-one');
+        }
         // perform animated scrolling by getting top-position of target-element and set it as scroll target
         $('html, body')
             .stop()
@@ -35,31 +51,40 @@ $(window).on('load', e => {
     });
 });
 
-// $(document).on('scroll', e => {
-//     if ($('[a-aboutus]').offset().top - $(window).scrollTop() <= 0) {
-//         headerElem
-//             .addClass('active')
-//             .parent()
-//             .addClass('active');
-//     } else {
-//         headerElem
-//             .removeClass('active')
-//             .parent()
-//             .removeClass('active');
-//     }
+// For Displaying the hamburger in mobile view
+(e => {
+    hamburger.addEventListener('click', e => {
+        e.preventDefault();
+        listElem.classList.toggle('active');
+        document.querySelector('.icon-one').classList.toggle('active-one');
+    });
+})();
 
-//     trackerElem.each(function() {
-//         const scrollPos = $(document).scrollTop();
-//         const currLink = $(this);
-//         const refElement = $(currLink.attr('a-tracker'));
-//         if (
-//             refElement.position().top <= scrollPos &&
-//             refElement.position().top + refElement.height() > scrollPos
-//         ) {
-//             refElement.hasClass('active').removeClass('active');
-//             currLink.addClass('active');
-//         } else {
-//             currLink.removeClass('active');
-//         }
-//     });
-// });
+$(document).on('scroll', e => {
+    if ($('[a-aboutus]').offset().top - $(window).scrollTop() <= 0) {
+        headerElem
+            .addClass('active')
+            .parent()
+            .addClass('active');
+    } else {
+        headerElem
+            .removeClass('active')
+            .parent()
+            .removeClass('active');
+    }
+
+    // trackerElem.each(function() {
+    //     const scrollPos = $(document).scrollTop();
+    //     const currLink = $(this);
+    //     const refElement = $(currLink.attr('a-tracker'));
+    //     if (
+    //         refElement.position().top <= scrollPos &&
+    //         refElement.position().top + refElement.height() > scrollPos
+    //     ) {
+    //         refElement.hasClass('active').removeClass('active');
+    //         currLink.addClass('active');
+    //     } else {
+    //         currLink.removeClass('active');
+    //     }
+    // });
+});
